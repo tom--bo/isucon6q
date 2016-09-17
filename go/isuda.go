e281a14869c1c650dd7fd810708b368b69564c6c
Ubuntu <isucon@isucon6-qualifier.msldsjzsronupa0pu03gtx2n1d.lx.internal.cloudapp.net>
2016-09-17 12:31:01 +0900

    replace RGEX to string.replace

--- go/isuda.go
+++ go/isuda.go
@@ -326,12 +326,21 @@ func htmlify(w http.ResponseWriter, r *http.Request, content string) string {
 	for _, entry := range entries {
 		keywords = append(keywords, regexp.QuoteMeta(entry.Keyword))
 	}
-	re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
+	/*
+		re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
+		kw2sha := make(map[string]string)
+		content = re.ReplaceAllStringFunc(content, func(kw string) string {
+			kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
+			return kw2sha[kw]
+		})
+	*/
 	kw2sha := make(map[string]string)
-	content = re.ReplaceAllStringFunc(content, func(kw string) string {
-		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-		return kw2sha[kw]
-	})
+	for _, kw := range keywords {
+
+		hash := "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
+		kw2sha[kw] = hash
+		content = strings.Replace(content, kw, hash, -1)
+	}
 	content = html.EscapeString(content)
 	for kw, hash := range kw2sha {
 		u, err := r.URL.Parse(baseUrl.String() + "/keyword/" + pathURIEscape(kw))
